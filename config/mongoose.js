bdde9b4e3e330e148d2b978671b092b45075edf4
Sachinsb18 <sachinsb.08@gmail.com>
2023-11-10 15:31:36 +0530

    config file updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -26,24 +26,3 @@ db.once('open', function () {
 
 
 
-// // require .env
-// require('dotenv').config()
-
-// // require mongoose
-// const mongoose = require('mongoose');
-
-// //  connect mongoose
-// const connectToMongoose = async () => {
-//     try { 
-//             await mongoose.connect('mongodb://127.0.0.1:27017/Habit_Tracker',{
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         console.log('connected to MongoDB');
-//     }catch(err){
-//         console.log(err);
-//     }
-// };
-
-
-// module.exports = connectToMongoose;
